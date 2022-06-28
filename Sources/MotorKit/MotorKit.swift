dbd4ab64c83cf48d0891c4cebdfed82d14fd5b66
Prad Nukala <pradn@sonr.io>
2022-06-28 11:59:48 -0400

    Update MotorKit method

--- Sources/MotorKit/MotorKit.swift
+++ Sources/MotorKit/MotorKit.swift
@@ -4,26 +4,33 @@ import Foundation
 @objc(MotorKit)
 class MotorKit : NSObject {
     private(set) var hasWallet = false
-    
-
     @objc static func requiresMainQueueSetup() -> Bool {
-        return false
+        return true
+    }
+    
+    @objc
+    func constantsToExport() -> [AnyHashable : Any]! {
+      return [
+        "number": 123.9,
+        "string": "foo",
+        "boolean": true,
+        "array": [1, 22.2, "33"],
+        "object": ["a": 1, "b": 2]
+      ]
     }
     
     @objc func newWallet() -> Bool {
-        var result = false;
-        DispatchQueue.global(qos: .background).sync {
-          result = self._newWallet()
+        if self.hasWallet {
+            print("Wallet has already been created")
+            return false
         }
-        return result;
-    }
-    
-    func _newWallet() -> Bool {
+        
         let error: NSErrorPointer = nil
         let resp = SNRMotorNewWallet(error)
         if error != nil {
             return false
         }
+        self.hasWallet = resp
         return resp
     }
 }
